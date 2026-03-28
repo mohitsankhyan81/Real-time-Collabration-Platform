e4618255c4fc98ffd49501e4e83984af62499aab
mohitsankhyan81 <mr.bambuop3344@gmail.com>
2026-03-28 20:14:02 +0530

    baisic file structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='bg-red-400 px-8 py-20 mt-4 text-yellow-300 text-center'>Hello</h1>
+    </div>
+  )
+}
+
+export default App
